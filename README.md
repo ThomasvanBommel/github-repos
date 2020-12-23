7606684355a66fa480136fe877b0532ec7dcafc4
Thomas vanBommel <thomasvanbommel@hotmail.com>
2020-12-22 21:46:37 -0400

    Modified options parameter and added documentation

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # github-repos
 
 Express middleware to grab repository information from GitHubs REST API.
+
+Uses GitHub API to pull repositories sorted and orders in a specified way.
+
+## Params
+```js
+/**
+ * Grab GitHub repositories
+ * @param {Object} options - GitHubRepos options
+ * @param {string} options.user - GitHub username (User Agent)
+ * @param {string} options.token - GitHub API access token
+ * @param {string} [options.sort="updated"] - How to sort repositories
+ * @param {string} [options.order="desc"] - How to order repositories
+ * @param {integer} [options.interval=3600000] - Update interval (default 1h)
+ */
+```
+
+## Usage
+
+```js
+const GitHub = require("./modules/github-repos/github-repos");
+const express = require("express");
+
+let app = express();
+
+app.use(GitHub({
+  user: "ThomasvanBommel",
+  token: "authentication-token"
+}));
+
+app.get("*", (req, res) => {
+  console.log(req.github_repos);
+});
+
+app.listen(8000);
+```
--- github-repos.js
+++ github-repos.js
@@ -6,40 +6,90 @@
 const https = require("https");
 
 class GitHubRepos {
+  /**
+   * Grab GitHub repositories
+   * @param {Object} options - GitHubRepos options
+   * @param {string} options.user - GitHub username (User Agent)
+   * @param {string} options.token - GitHub API access token
+   * @param {string} [options.sort="updated"] - How to sort repositories
+   * @param {string} [options.order="desc"] - How to order repositories
+   * @param {integer} [options.interval=3600000] - Update interval (default 1h)
+   */
   constructor(options){
+    // ensure proper options object
+    if(!options.user) throw "Missing options.user...";
+    if(!options.token) throw "Midding options.token...";
+
+    // add sort + order if required
+    if(!options.sort) options.sort = "updated";
+    if(!options.order) options.order = "desc";
+
+    // set up github api options
+    options.hostname = "api.github.com";
+    options.port = 443;
+    options.path = `/user/repos?sort=${options.sort}&order=${options.order}`;
+    options.method = "GET";
+    options.headers = {
+      "User-Agent": options.user,
+      "Authorization": `token ${options.token}`
+    };
+
+    // add options to this object + set repos to empty list
     this.options = options;
     this.repos = [];
 
+    // update once, and again every (default 1h)
     this.update();
     setInterval(this.update, options.interval ? options.interval : 3600000);
   }
 
+  /** Update repositories */
   update = () => {
+    // create request
     let req = https.request(this.options, res => {
-      let data = "";
+      // get number of remaining api calls
       let remaining = res.headers["x-ratelimit-remaining"];
+      let data = ""; // accumulative data
 
+      // log headers if they are not "200 OK"
       if(res.headers.status != "200 OK") console.log(res.headers);
 
+      // accumulate data
       res.on("data", d => { data += d; });
+
+      // end of transmission; parse data
       res.on("end", () => {
         this.repos = JSON.parse(data);
         console.log(`${new Date().toISOString()} Updated GitHub Repositories (remaining: ${remaining})`);
       });
     });
 
+    // log errors and send request
     req.on("error", err => { console.error(err); });
-
     req.end();
   };
 
+  /**
+   * Express middleware (adds repos to req.github_repos)
+   * @param {Object} req - Express request "req" object
+   * @param {Object} res - Express response "res" object
+   * @param {Object} next - Express "next" middleware object
+   */
   middleware = (req, res, next) => {
     req.github_repos = this.repos;
     next();
   };
 }
 
-
+/**
+ * Grab GitHub repositories
+ * @param {Object} options - GitHubRepos options
+ * @param {string} options.user - GitHub username (User Agent)
+ * @param {string} options.token - GitHub API access token
+ * @param {string} [options.sort="updated"] - How to sort repositories
+ * @param {string} [options.order="desc"] - How to order repositories
+ * @param {integer} [options.interval=3600000] - Update interval (default 1h)
+ */
 module.exports = options => {
   return new GitHubRepos(options).middleware;
 };
